c090c96c7add6137b7bb5699ae5352dd5894ed4f
Ruud Senden <ruud.senden@microfocus.com>
2020-03-12 12:27:39 +0100

    Added functionality for creating SSC attribute definitions

--- client-api-ssc/src/main/lombok/com/fortify/client/ssc/api/SSCAttributeDefinitionAPI.java
+++ client-api-ssc/src/main/lombok/com/fortify/client/ssc/api/SSCAttributeDefinitionAPI.java
@@ -24,17 +24,27 @@
  ******************************************************************************/
 package com.fortify.client.ssc.api;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.HttpMethod;
+import javax.ws.rs.client.Entity;
+
+import org.springframework.util.Assert;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fortify.client.ssc.api.query.builder.SSCAttributeDefinitionsQueryBuilder;
 import com.fortify.client.ssc.connection.SSCAuthenticatingRestConnection;
 import com.fortify.util.rest.json.JSONList;
 import com.fortify.util.rest.json.JSONMap;
+
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
 /**
  * This class is used to access SSC attribute-related functionality.
@@ -43,6 +53,21 @@ import com.fortify.util.rest.json.JSONMap;
  *
  */
 public class SSCAttributeDefinitionAPI extends AbstractSSCAPI {
+	public static enum SSCAttributeDefinitionCategory {
+		TECHNICAL, ORGANIZATION, BUSINESS, DYNAMIC_SCAN_REQUEST
+	}
+	
+	// TODO Differentiate between types supported by dynamic scan request vs other ctaegories
+	// Technical/Organization/Business support all types except FILE and SENSITIVE_TEXT
+	// Dynamic scan request supports all types except DATE and INTEGER
+	public static enum SSCAttributeDefinitionType {
+		TEXT, LONG_TEXT, SENSITIVE_TEXT, SINGLE, MULTIPLE, BOOLEAN, INTEGER, DATE, FILE
+	}
+	
+	public static enum SSCAttributeDefinitionAppEntityType {
+		PROJECT_VERSION, DYNAMIC_SCAN_REQUEST, ALL
+	}
+	
 	public SSCAttributeDefinitionAPI(SSCAuthenticatingRestConnection conn) {
 		super(conn);
 	}
@@ -53,6 +78,10 @@ public class SSCAttributeDefinitionAPI extends AbstractSSCAPI {
 	
 	public JSONList getAttributeDefinitions(boolean useCache, String... fields) {
 		return queryAttributeDefinitions().useCache(useCache).paramFields(fields==null?null:fields).build().getAll();
+	}
+	
+	public SSCCreateAttributeDefinitionBuilder createAttributeDefinition() {
+		return new SSCCreateAttributeDefinitionBuilder(conn());
 	}
 	
 	public String getAttributeIdForName(boolean useCache, String attributeName) {
@@ -101,6 +130,50 @@ public class SSCAttributeDefinitionAPI extends AbstractSSCAPI {
 		JSONMap attributeDefinitionsAndNameOrId = attributeDefinitions.toJSONMap("name", String.class, "#this", JSONMap.class);
 		attributeDefinitionsAndNameOrId.putAll(attributeDefinitions.toMap("id", String.class, JSONMap.class));
 		return attributeDefinitionsAndNameOrId;
+	}
+	
+	@Setter @Accessors(fluent=true)
+	public static final class SSCCreateAttributeDefinitionBuilder {
+		private final SSCAuthenticatingRestConnection conn;
+		@JsonProperty private String name;
+		@JsonProperty private String description = "";
+		@JsonProperty private boolean required = false;
+		@JsonProperty private boolean hidden = false;
+		@JsonProperty private SSCAttributeDefinitionCategory category = SSCAttributeDefinitionCategory.TECHNICAL;
+		@JsonProperty private SSCAttributeDefinitionType type = SSCAttributeDefinitionType.TEXT;
+		@JsonProperty private SSCAttributeDefinitionAppEntityType appEntityType = SSCAttributeDefinitionAppEntityType.PROJECT_VERSION;
+		@JsonProperty private List<SSCAttributeDefinitionOption> options;
+		
+		SSCCreateAttributeDefinitionBuilder(SSCAuthenticatingRestConnection conn) {
+			this.conn = conn;
+		}
+		
+		public SSCCreateAttributeDefinitionBuilder option(SSCAttributeDefinitionOption option) {
+			if ( options==null ) { options = new ArrayList<>(); }
+			options.add(option);
+			return this;
+		}
+		
+		public JSONMap execute() {
+			Assert.notNull(name, "Attribute definition name must be specified");
+			if ( !SSCAttributeDefinitionType.SINGLE.equals(type) && !SSCAttributeDefinitionType.MULTIPLE.equals(type) ) {
+				Assert.isNull(options, "Options are only supported for type SINGLE or MULTIPLE");
+			}
+			return conn.executeRequest(HttpMethod.POST, 
+					conn.getBaseResource().path("/api/v1/attributeDefinitions"), 
+					Entity.entity(this, "application/json"), JSONMap.class);	
+		}
+		
+		@Setter @Accessors(fluent=true)
+		public static final class SSCAttributeDefinitionOption {
+			@JsonProperty private final String name;
+			@JsonProperty private String description;
+			@JsonProperty private boolean hidden;
+			
+			public SSCAttributeDefinitionOption(String name) {
+				this.name = name;
+			}
+		}
 	}
 
 }
