0e201a1aa3c241f0702f46f1ff9159183cd165ba
rsenden <ruud.senden@microfocus.com>
2020-03-18 00:47:44 +0100

    Updated fortify-scan.sh

--- fortify-scan.sh
+++ fortify-scan.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-projectName=fortify-client-api
+projectName=fortify-ssc-parser-util
+# Scan the project as a module (SCA 19.2+ feature)
 scanOpts="-scan-module -Dcom.fortify.sca.UseSynchronousSerialization=true -Dcom.fortify.sca.ThreadCount=1 -Dcom.fortify.sca.DefaultAnalyzers=dataflow"
 
 set -x
@@ -12,7 +13,7 @@ sourceanalyzer -b ${projectName} -clean
 # The -Pfortify option informs our build script that 
 # we're running a Fortify translation, allowing the
 # build script to take appropriate measures if necessary
-sourceanalyzer -b ${projectName} gradle clean build -Pfortify
+sourceanalyzer -b ${projectName} gradle clean build -Pfortify 
 
-# Scan the project as a module (SCA 19.2+ feature)
+# Scan the project using scan options specified in ${scanOpts}
 sourceanalyzer -b ${projectName} -f ${projectName}.fpr ${scanOpts}
